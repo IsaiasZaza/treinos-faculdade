47e1b20ad2adef32d8b0035a5f2fe5c0c29ba944
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-05-17 23:26:08 -0300

    treino java
    treino java

---
+++ treino js/script.js
@@ -0,0 +1,15 @@
+let idade = 90
+let nome = "Isaias Gonçalves"
+let cidade = "Brasilia"
+let estado = "DF"
+let da = "BH"
+let logado = false
+let lista = ["ovo", "massa", "corante"]
+let NomeCompleto = { nome: "zaza", idade: 90 }
+
+console.log(typeof lista)
+console.log(idade)
+console.log(nome)
+console.log(cidade)
+console.log(estado)
+console.log(da)
