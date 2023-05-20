c93c5df3ed6077bd2842aff669d2b26ecc364025
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-05-19 23:55:23 -0300

    aula 2 de java
    Operações variaveis

--- treino js/script.js
+++ treino js/script.js
@@ -1,11 +1,14 @@
-let idade = 90
-let nome = "Isaias Gonçalves"
-let cidade = "Brasilia"
-let estado = "DF"
-let da = "BH"
-let logado = false
-let lista = ["ovo", "massa", "corante"]
-let NomeCompleto = { nome: "zaza", idade: 90 }
+/**Aula 1 **/
+/**let idade = 90 //number
+let cidae = "Brasilia"//string
+let estado = "DF"//string
+let da = "BH"//string
+let logado = false//boolean
+let lista = ["ovo", "massa", "corante"]//array
+let NomeCompleto = { nome: "zaza", idade: 90 }//object 
+let cidade = "Sao Paulo"
+
+let nome = { nome: "Ferrari", valor: 15555 }
 
 console.log(typeof lista)
 console.log(idade)
@@ -13,3 +16,20 @@ console.log(nome)
 console.log(cidade)
 console.log(estado)
 console.log(da)
+**/
+
+/*Aula 2 */
+let x = "8";
+
+x = x + 8
+
+console.log(x)
+
+let nome = 'Isaias';
+let sobrenome = 'Gonçalves';
+let nomeCompleto = nome + ' ' + sobrenome
+
+console.log(nomeCompleto)
+
+
+
