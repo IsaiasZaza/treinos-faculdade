9b6b4aac905eb9359d4909ed5284013ebd657210
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-05-25 10:33:09 -0300

    aula 17 java script
    treinando condicionais.

--- treino js/script.js
+++ treino js/script.js
@@ -1,32 +1,38 @@
-//let nome = "Isaias"
-//let sobrenome = "Gonçalves"
-//let NomeCompleto = nome + ' ' + sobrenome;
-//let nomeCompleto = `${nome} ${sobrenome} da Silva`;
+/*let nome = "Isaias"
+let sobrenome = "Gonçalves"
+let NomeCompleto = nome + ' ' + sobrenome;
+let nomeCompleto = `${nome} ${sobrenome} da Silva`;
+console.log("Nome: " + nomeCompleto);
 
-//console.log("Nome: " + nomeCompleto);
+let idade = 90;
+let idadeString = `${idade + idade} anos`
+console.log(idadeString)
+let idade = 17;
 
-//let idade = 90;
-//let idadeString = `${idade + idade} anos`
-//console.log(idadeString)
-//let idade = 17;
+if (idade >= 17) {  
+    console.log("voce é maior de idade")
+} else{
+    console.log("voce é menor de idade")
+}
+let idade = 20;
 
-//if (idade >= 17) { // voce pode inverter a string com o number e fazer sinais >< para os dois lados;
-  //  console.log("voce é maior de idade")
-//} else{
-//    console.log("voce é menor de idade")
-//}
-//let idade = 20;
-
-//if (idade == 20){
+if (idade == 20){
   //  console.log("voce tem 20 anos")
-//} else{ 
-    //console.log("voce nao tem 20 anos")
-//}
+} else{ 
+    console.log("voce nao tem 20 anos")
+}
 
 let idade = 28
 if (idade === 28){
     console.log("voce tem 28 anos");
     idade = idade + 35;
-    console.log("voce tem agora muitos anos kk 65")
     console.log(idade);
-} 
+    console.log("voce tem agora muitos anos kk 63")
+    
+} */
+let idade = 28
+if (idade >= 25 && idade <30) {
+  console.log("Voce tem 25");
+}else {
+  console.log("Voce é um bebe")
+}
