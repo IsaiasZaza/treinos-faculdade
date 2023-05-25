2805d5be1087effb06479c01eb37b7b14fb24e14
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-05-25 09:25:19 -0300

    Trabalho front end
    deixei os avisos no proprio comando 25/05

--- treino js/script.js
+++ treino js/script.js
@@ -1,35 +1,32 @@
-/**Aula 1 **/
-/**let idade = 90 //number
-let cidae = "Brasilia"//string
-let estado = "DF"//string
-let da = "BH"//string
-let logado = false//boolean
-let lista = ["ovo", "massa", "corante"]//array
-let NomeCompleto = { nome: "zaza", idade: 90 }//object 
-let cidade = "Sao Paulo"
+//let nome = "Isaias"
+//let sobrenome = "Gonçalves"
+//let NomeCompleto = nome + ' ' + sobrenome;
+//let nomeCompleto = `${nome} ${sobrenome} da Silva`;
 
-let nome = { nome: "Ferrari", valor: 15555 }
+//console.log("Nome: " + nomeCompleto);
 
-console.log(typeof lista)
-console.log(idade)
-console.log(nome)
-console.log(cidade)
-console.log(estado)
-console.log(da)
-**/
+//let idade = 90;
+//let idadeString = `${idade + idade} anos`
+//console.log(idadeString)
+//let idade = 17;
 
-/*Aula 2 */
-let x = "8";
+//if (idade >= 17) { // voce pode inverter a string com o number e fazer sinais >< para os dois lados;
+  //  console.log("voce é maior de idade")
+//} else{
+//    console.log("voce é menor de idade")
+//}
+//let idade = 20;
 
-x = x + 8
+//if (idade == 20){
+  //  console.log("voce tem 20 anos")
+//} else{ 
+    //console.log("voce nao tem 20 anos")
+//}
 
-console.log(x)
-
-let nome = 'Isaias';
-let sobrenome = 'Gonçalves';
-let nomeCompleto = nome + ' ' + sobrenome
-
-console.log(nomeCompleto)
-
-
-
+let idade = 28
+if (idade === 28){
+    console.log("voce tem 28 anos");
+    idade = idade + 35;
+    console.log("voce tem agora muitos anos kk 65")
+    console.log(idade);
+} 
