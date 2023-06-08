3f487cd3bc0f614cf90bf4a3c8d0e47b6184217e
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-08 00:29:41 -0300

    Estudou udemy
    parte 1!

---
+++ curso udemy-java/2-operação aritimetica/script.js
@@ -0,0 +1,5 @@
+console.log(typeof (2 + 90));
+console.log(typeof(5 - 12));
+console.log(typeof (50 / 10));
+console.log(typeof (10 * 10));
+console.log(typeof (20 % 10));
---
+++ curso udemy-java/5-string avançado/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nSegunda Linha");
+console.log("O meu nome é 'Isaias'")
+console.log('O nome dela é "Rafaella"')
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O" + " meu " + " é " + 'Matheus.')
