564c18540fdcb54b284e1721979b20a1549ca60a
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-10 11:35:40 -0300

    estudo java
    ...

---
+++ curso udemy-java copy/8-boolean-oplogic copy 2/script.js
@@ -0,0 +1,3 @@
+console.log (5 > 2 ? 'é sim' : 'é nao');
+console.log(false ? 5 : 4);
+console.log("Isaias" == "Isaas" ? "Olá zaza" : "Olá estranho");
---
+++ curso udemy-java copy/8-boolean-oplogic/script.js
@@ -0,0 +1,7 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+console.log(3 == 3 && 'Mateus' == 'Mateus');
+console.log("Felipe" == "Joao" || false);
+console.log(!(true && true));
+console.log(true && true);
+console.log(false || false);
