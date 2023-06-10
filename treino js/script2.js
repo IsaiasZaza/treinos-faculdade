b69f11c6c88d6859fba0a188a525ff4c962f8f23
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-10 09:45:43 -0300

    treinos facul
    dia 10/06

---
+++ treino js/script2.js
@@ -0,0 +1,11 @@
+//Seleção
+const generatorPasswordButton = document.querySelector("#generate-password")
+const generatorPasswordElement = document.querySelector("#generated-password")
+
+//Funçoes
+// Letras, Números e Símbolos
+"abcde"
+//Eventos
+generatorPasswordButton.addEventListener("click", () => {
+    console.log("teste")
+});
