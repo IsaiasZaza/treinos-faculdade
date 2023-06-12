e758ad253b03ed03c01d108ffa9003c11b832a72
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-12 09:20:10 -0300

    aula dia 12/06

---
+++ curso udemy-java copy/12 aulas de built in/script.js
@@ -0,0 +1,17 @@
+alert('essa é eu');
+
+//let idade = prompt("Qual a sua Idade");
+//console.log(idade);
+
+/*let nome = prompt("Qual seu nome?");
+console.log(`O seu nome é ${nome}`)*/
+
+let maior = Math.max(6.12,16,25,64);
+console.log(maior);
+
+let menor = Math.min(0.5, 0.4);
+console.log(menor);
+let arredondar = Math.round(5.2934723);
+console.log(arredondar);
+let arredoncima = Math.ceil(5.3546346546);
+console.log(arredoncima);
