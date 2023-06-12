918e8be9ba90caf6e84b0638589136f64fea77ce
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-12 08:23:34 -0300

    aula do dia 11/06

---
+++ curso udemy-java copy/11-variaveis/script.js
@@ -0,0 +1,13 @@
+let nome = 'Matheus';
+console.log(nome);
+console.log(`o meu nome é ${nome}`);
+
+let laranjas = 5;
+console.log(laranjas * laranjas);
+laranjas = 894984;
+nome = "lucas";
+console.log(nome, laranjas);
+
+const ip = "127.0.0.1";
+console.log(ip);
+
--- curso udemy-java copy/9-valores vazios/script.js
+++ curso udemy-java copy/9-valores vazios/script.js
@@ -1,2 +1,12 @@
-console.log(null);
-console.log(undefined);
+console.log('Isaias Goncalves')
+console.log('Gomes Isaias')
+console.log('Zaza Gomes')
+console.log(21313)
+console.log(102030)
+console.log(10 * 3)
+console.log(5 < 2)
+console.log(3 > 2)
+console.log ("Isaias" === "zaza")
+console.log( 5 < 3 && 5 < 3);
+console.log( 5 > 3 || 5 > 2);
+console.log(!(2 === 2 ));
