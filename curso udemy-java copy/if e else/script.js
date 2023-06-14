f8c3708f40e867fa8511df3126fdda7038a5025a
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-13 22:34:05 -0300

    aula 13/06
    estudo de hoje loop

--- curso udemy-java copy/if e else/script.js
+++ curso udemy-java copy/if e else/script.js
@@ -1,36 +1,14 @@
-let idade = 18;
-if(idade == 19){
-    console.log("A idade é igual a 19");
+let x = 10;
+while(x > 0){
+    console.log("o x é " + x);
+    x = x - 1;
 }
-if (idade > 25){
-    console.log("A idade é maior que 25");
+let y = 0;
+while( y <= 10){
+    console.log(y)
+    y = y + 1;
 }
-
-let passaporte = true;
-
-if((name == "Matheus" && idade > 30) || passaporte == true);{
-    console.log("Liberado1!")
-}
-
-let velocidade = 50;
-if(velocidade < 80){
-    console.log('não foi multado')
-} else {
-    console.log('Voce foi multado!')
-}
-
-let power = 800;
-if(power < 799){
-    console.log("voce é forte");
-} else {
-    console.log("voce é fraco");
-}
-idade = 90;;
-nome = "Rafaella"; 
-if(nome == "Lucas"){
-    console.log("Incorreto")
-}else if(nome === "Rafaella" && idade === 90){
-    console.log("Voce é linda")
-}else{
-    console.log("Nem te conheço")
+//while, for
+for(let i = 0; i < 100; i = i) {
+    console.log(`A soma de i com 2 é: ${i}`)
 }
---
+++ curso udemy-java copy/loop Js/script.js
@@ -0,0 +1,36 @@
+let idade = 18;
+if(idade == 19){
+    console.log("A idade é igual a 19");
+}
+if (idade > 25){
+    console.log("A idade é maior que 25");
+}
+
+let passaporte = true;
+
+if((name == "Matheus" && idade > 30) || passaporte == true);{
+    console.log("Liberado1!")
+}
+
+let velocidade = 50;
+if(velocidade < 80){
+    console.log('não foi multado')
+} else {
+    console.log('Voce foi multado!')
+}
+
+let power = 800;
+if(power < 799){
+    console.log("voce é forte");
+} else {
+    console.log("voce é fraco");
+}
+idade = 90;;
+nome = "Rafaella"; 
+if(nome == "Lucas"){
+    console.log("Incorreto")
+}else if(nome === "Rafaella" && idade === 90){
+    console.log("Voce é linda")
+}else{
+    console.log("Nem te conheço")
+}
