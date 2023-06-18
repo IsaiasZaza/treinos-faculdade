27db92227953aedfdf7688e143d2c2a269bc4f65
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-06-18 17:40:17 -0300

    estudo java 18/06

---
+++ curso udemy-java copy/19 -exercicio tipos de dados/script.js
@@ -0,0 +1,41 @@
+/*let nome = "Isaias";
+let number =  15;
+let boolean = true;
+console.log(typeof nome)
+console.log(typeof number)
+console.log(typeof boolean)*/
+
+let idade = 35;
+if(idade > 18 && idade < 60){
+    console.log('pode entrar')
+}
+//const nome = "Isaias";
+/*if(nome == "Isaias" && nome == "Isaia"){
+    console.log("Ola Isaias")
+}*/
+const num = 18;
+console.log(Math.pow(2,2))
+console.log(Math.pow(3,2))
+console.log(Math.pow(num,2))
+let velocidade = 90;
+if(velocidade > 0 && velocidade < 80){
+    console.log("voce nao levou tua multa zé")
+}else{
+    console.log("Ta safe")
+}
+
+let CNH = true;
+let nome = 'Isaias';
+let years = 15;
+if(CNH == true && nome == "Isaac" && years == 0){
+    console.log("Voce nao é o Isaias, ESTA PRESO")
+}else if(CNH == true && nome == "Isaias" && years == 18){
+    console.log("Voce passou negao")
+}else{
+    console.log("deu ruim pra ti, nem um nem outro")
+}
+let i = 0;
+/*while( i <= 10){
+    console.log(i);
+    i++;
+}*/
