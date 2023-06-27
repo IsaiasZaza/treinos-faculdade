e1d8c04d33ca8bf2f8fe09a6b692c50d1d5706ad
Isaias Gonçalves <isaias.legend@gmail.com>
2023-06-27 17:54:27 -0300

    estudos javascript

---
+++ curso udemy-java copy/28-array/script.js
@@ -0,0 +1,19 @@
+let arr = [1,4,5,6,7]
+let nomes = ["Matheus", "Falso", "Ricardo"]
+let bool = [true, false, true, false]
+let misturado = [1, "matheus", true]
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[3]);
+console.log(misturado[0]);
+console.log(arr[arr.length -1])
+
+/*propriedades*/
+
+let nome = "Joaaaaaaso"
+console.log(nome.length)
+
+let numeros = [1,23,2,25,87]
+console.log(numeros.length)
+console.log(numeros[2])
